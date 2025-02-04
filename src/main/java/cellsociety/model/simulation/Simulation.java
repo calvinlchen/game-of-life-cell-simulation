c48f9a6dc199c05c7f4c5979d7252d446d4f8938
Calvin Chen <clc162@duke.edu>
2025-02-04 03:05:00 -0500

    Fix: create Cell objects (based on simulation type) within a List, in order to pass Cells into new Grids. (fulfills one of the constructor requirements for Grid.)

--- src/main/java/cellsociety/model/simulation/Simulation.java
+++ src/main/java/cellsociety/model/simulation/Simulation.java
@@ -2,9 +2,27 @@ package cellsociety.model.simulation;
 
 import cellsociety.model.interfaces.Cell;
 import cellsociety.model.interfaces.Grid;
+import cellsociety.model.simulation.cell.GameOfLifeCell;
+import cellsociety.model.simulation.cell.FireCell;
+import cellsociety.model.simulation.cell.PercolationCell;
+import cellsociety.model.simulation.cell.SegregationCell;
+import cellsociety.model.simulation.cell.WaTorCell;
 import cellsociety.model.simulation.grid.AdjacentGrid;
 import cellsociety.model.simulation.grid.RectangularGrid;
+import cellsociety.model.simulation.rules.FireRule;
+import cellsociety.model.simulation.rules.GameOfLifeRule;
+import cellsociety.model.simulation.rules.PercolationRule;
+import cellsociety.model.simulation.rules.SegregationRule;
+import cellsociety.model.simulation.rules.WaTorRule;
 import cellsociety.model.util.XMLData;
+import cellsociety.model.util.constants.CellStates.FireStates;
+import cellsociety.model.util.constants.CellStates.GameOfLifeStates;
+import cellsociety.model.util.constants.CellStates.PercolationStates;
+import cellsociety.model.util.constants.CellStates.SegregationStates;
+import cellsociety.model.util.constants.CellStates.WaTorStates;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class Simulation<S extends Enum<S>, T extends Cell<S, T>> {
   private final XMLData xmlData;
@@ -17,19 +35,36 @@ public class Simulation<S extends Enum<S>, T extends Cell<S, T>> {
   }
 
   private void setUpGrid() {
+    List<Cell> cellList = new ArrayList<>();
+    Map<String, Double> params = xmlData.getParameters();
+    for (Enum state : xmlData.getCellStateList()) {
+      switch (xmlData.getType()) {
+        case GAMEOFLIFE -> cellList.add(new GameOfLifeCell((GameOfLifeStates) state,
+            new GameOfLifeRule(params)));
+        case SEGREGATION -> cellList.add(new SegregationCell((SegregationStates) state,
+            new SegregationRule(params)));
+        case FIRE -> cellList.add(new FireCell((FireStates) state,
+            new FireRule(params)));
+        case PERCOLATION -> cellList.add(new PercolationCell((PercolationStates) state,
+            new PercolationRule(params)));
+        case WATOR -> cellList.add(new WaTorCell((WaTorStates) state,
+            new WaTorRule(params)));
+      }
+    }
+
     switch (xmlData.getType()) {
-      case GAMEOFLIFE, SEGREGATION -> setUpRectangularGrid();
-      case FIRE, PERCOLATION, WATOR -> setUpAdjacentGrid();
+      case GAMEOFLIFE, SEGREGATION -> setUpRectangularGrid(cellList);
+      case FIRE, PERCOLATION, WATOR -> setUpAdjacentGrid(cellList);
     }
   }
 
-  private void setUpAdjacentGrid() {
-    myGrid = new AdjacentGrid(xmlData.getCellStateList(), xmlData.getGridRowNum(),
+  private void setUpAdjacentGrid(List<Cell> cellList) {
+    myGrid = new AdjacentGrid(cellList, xmlData.getGridRowNum(),
         xmlData.getGridColNum());
   }
 
-  private void setUpRectangularGrid() {
-    myGrid = new RectangularGrid(xmlData.getCellStateList(), xmlData.getGridRowNum(),
+  private void setUpRectangularGrid(List<Cell> cellList) {
+    myGrid = new RectangularGrid(cellList, xmlData.getGridRowNum(),
         xmlData.getGridColNum());
   }
 
