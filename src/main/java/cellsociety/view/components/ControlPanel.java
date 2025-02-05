4e248b2e7c792a5567f8eb6249fbc2f3ff29de40
Calvin Chen <clc162@duke.edu>
2025-02-05 01:12:40 -0500

    Refactor DESIGN-16: Remove unused instance variables by including all buttons in a List

--- src/main/java/cellsociety/view/components/ControlPanel.java
+++ src/main/java/cellsociety/view/components/ControlPanel.java
@@ -1,5 +1,7 @@
 package cellsociety.view.components;
 
+import java.util.ArrayList;
+import java.util.List;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -10,43 +12,45 @@ import javafx.scene.layout.VBox;
 public class ControlPanel {
   private final VBox myPanel;
   private final UserView myUserView;
-  private Button myPlayButton;
-  private Button myPauseButton;
-  private Button myStopButton;
-  private Button myLoadButton;
-  private Button mySaveButton;
-  private Button myRandomButton;
-  private HBox mySpeedPanel;
+  private final List<Button> myButtons;
 
   public ControlPanel(UserView userView) {
     myUserView = userView;
     myPanel = new VBox(12);
+    myButtons = new ArrayList<>();
     initializeControls();
   }
 
   private void initializeControls() {
-    myPlayButton = new Button("Play");
-    myPlayButton.setOnAction(e -> myUserView.playSimulation());
+    Button playButton = new Button("Play");
+    playButton.setOnAction(e -> myUserView.playSimulation());
+    myButtons.add(playButton);
 
-    myPauseButton = new Button("Pause");
-    myPauseButton.setOnAction(e -> myUserView.pauseSimulation());
+    Button pauseButton = new Button("Pause");
+    pauseButton.setOnAction(e -> myUserView.pauseSimulation());
+    myButtons.add(pauseButton);
 
-    myStopButton = new Button("Clear Grid");
-    myStopButton.setOnAction(e -> myUserView.stopAndResetSimulation());
+    Button clearButton = new Button("Clear Grid");
+    clearButton.setOnAction(e -> myUserView.stopAndResetSimulation());
+    myButtons.add(clearButton);
 
-    myLoadButton = new Button("Load New File");
-    myLoadButton.setOnAction(e -> myUserView.loadSimulation());
+    Button loadButton = new Button("Load New File");
+    loadButton.setOnAction(e -> myUserView.loadSimulation());
+    myButtons.add(loadButton);
 
-    mySaveButton = new Button("Save As");
-    mySaveButton.setOnAction(e -> myUserView.saveSimulation());
+    Button saveButton = new Button("Save As");
+    saveButton.setOnAction(e -> myUserView.saveSimulation());
+    myButtons.add(saveButton);
 
-    myRandomButton = new Button("Random Game of Life");
-    myRandomButton.setOnAction(e -> myUserView.loadRandomGameOfLife());
+    Button randomButton = new Button("Random Game of Life");
+    randomButton.setOnAction(e -> myUserView.loadRandomGameOfLife());
+    myButtons.add(randomButton);
 
-    mySpeedPanel = makeSpeedPanel();
+    myPanel.getChildren().addAll(myButtons);
 
-    myPanel.getChildren().addAll(myPlayButton, myPauseButton, myStopButton, myLoadButton,
-        mySaveButton, myRandomButton, mySpeedPanel);
+    // Panel containing buttons for speeding up / slowing down the simulations
+    HBox speedPanel = makeSpeedPanel();
+    myPanel.getChildren().addAll(speedPanel);
   }
 
   private HBox makeSpeedPanel() {
