818e7b7886c8a12f314129ac4d571aebc5d6f4f0
Calvin Chen <clc162@duke.edu>
2025-02-05 00:44:31 -0500

    Doc: add Javadoc comments to public (all) methods

--- src/main/java/cellsociety/view/components/InformationBox.java
+++ src/main/java/cellsociety/view/components/InformationBox.java
@@ -6,6 +6,9 @@ import javafx.scene.control.TextArea;
 public class InformationBox {
   private final TextArea myTextArea;
 
+  /**
+   * Creates the text box which displays current simulation info
+   */
   public InformationBox() {
     myTextArea = new TextArea();
     myTextArea.setPrefHeight(Main.SCENE_HEIGHT * (1 - UserView.GRID_PROPORTION_OF_SCREEN));
@@ -13,6 +16,10 @@ public class InformationBox {
     myTextArea.setEditable(false);
   }
 
+  /**
+   * Return text/information box view
+   * @return TextArea object
+   */
   public TextArea getTextArea() {
     return myTextArea;
   }
