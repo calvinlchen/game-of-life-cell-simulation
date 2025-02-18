f5c552116bd470582e4e23b1c330b7707241bf59
Calvin Chen <clc162@duke.edu>
2025-02-18 18:21:25 -0500

    Format: minor formatting changes for readability

--- src/main/java/cellsociety/model/simulation/parameters/GameOfLifeParameters.java
+++ src/main/java/cellsociety/model/simulation/parameters/GameOfLifeParameters.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class GameOfLifeParameters extends Parameters {
+
   private List<Integer> survive;
   private List<Integer> born;
 
@@ -14,25 +15,23 @@ public class GameOfLifeParameters extends Parameters {
     initializeDefaultParams();
   }
 
-  private void initializeDefaultParams() {
-    survive = new ArrayList<>(Arrays.asList(2, 3));
-    born = new ArrayList<>(List.of(3));
-  }
-
   public GameOfLifeParameters(String language) {
     super(language);
     initializeDefaultParams();
   }
 
+  private void initializeDefaultParams() {
+    survive = new ArrayList<>(Arrays.asList(2, 3));
+    born = new ArrayList<>(List.of(3));
+  }
+
   public GameOfLifeParameters(List<Integer> survive, List<Integer> born) {
     super();
-
     initializeParams(survive, born);
   }
 
   public GameOfLifeParameters(List<Integer> survive, List<Integer> born, String language) {
     super(language);
-
     initializeParams(survive, born);
   }
 
@@ -67,12 +66,13 @@ public class GameOfLifeParameters extends Parameters {
   /**
    * Validates the rules list.
    *
-   * @param rules - list to validate
+   * @param rules    - list to validate
    * @param errorKey - key for the error message in exceptions.properties
    */
   private void validateRules(List<Integer> rules, String errorKey) {
     if (rules == null) {
-      throw new SimulationException(String.format(getResources().getString("NullRuleList"), errorKey));
+      throw new SimulationException(
+          String.format(getResources().getString("NullRuleList"), errorKey));
     }
   }
 }
