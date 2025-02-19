c2069c6f020bba66102fb1771fb06c3b6ad2695d
Jessica Chen <jc939@duke.edu>
2025-02-19 12:39:31 -0500

    [Unfinished] pushing to slow current checks for petelka's rule

--- src/main/java/cellsociety/model/simulation/rules/PetelkaRule.java
+++ src/main/java/cellsociety/model/simulation/rules/PetelkaRule.java
@@ -61,39 +61,67 @@ public class PetelkaRule extends Rule<PetelkaCell, PetelkaParameters> {
   public int apply(PetelkaCell cell) {
     String[] directions = {"N", "NE", "E", "SE", "S", "SW", "W", "NW"};
 
-    for (int rotations = 0; rotations < 4; rotations++) {
-      String stateKey = getStateKey(cell, directions);
-
-      if (stateKey.length() != 9) {
-        return cell.getCurrentState();
-      }
-
-      if (RULES_MAP_PETELKA.containsKey(stateKey)) {
-        return RULES_MAP_PETELKA.get(stateKey);
-      }
-
-      // I think since i trying each rotation, then flipping horizontally gets all flips?
-      String reflectedKey = getStateKey(cell, reflectHorizontally(directions));
-      if (RULES_MAP_PETELKA.containsKey(reflectedKey)) {
-        return RULES_MAP_PETELKA.get(reflectedKey);
-      }
-
-      directions = rotateClockwise(directions);
+    String stateKey = getStateKey(cell, directions);
+    if (stateKey.length() != 9) {
+      return cell.getCurrentState();
     }
+
+    if (RULES_MAP_PETELKA.containsKey(stateKey)) {
+      return RULES_MAP_PETELKA.get(stateKey);
+    }
+
+//    e, se, s, sw, w, nw, n, ne
+    directions = new String[]{"E", "SE", "S", "SW", "W", "NW", "N", "NE"};
+    stateKey = getStateKey(cell, directions);
+    if (RULES_MAP_PETELKA.containsKey(stateKey)) {
+      return RULES_MAP_PETELKA.get(stateKey);
+    }
+
+//    s, sw, w, nw, n, ne, e, se
+    directions = new String[]{"S", "SW", "W", "NW", "N", "NE", "E", "SE"};
+    stateKey = getStateKey(cell, directions);
+    if (RULES_MAP_PETELKA.containsKey(stateKey)) {
+      return RULES_MAP_PETELKA.get(stateKey);
+    }
+
+//    w, nw, n, ne, e, se, s, sw
+    directions = new String[]{"W", "NW", "N", "NE", "E", "SE", "S", "SW"};
+    stateKey = getStateKey(cell, directions);
+    if (RULES_MAP_PETELKA.containsKey(stateKey)) {
+      return RULES_MAP_PETELKA.get(stateKey);
+    }
+
+//    s, se, e, ne, n, nw, w, sw
+    directions = new String[]{"S", "SE", "E", "NE", "N", "NW", "W", "SW"};
+    stateKey = getStateKey(cell, directions);
+    if (RULES_MAP_PETELKA.containsKey(stateKey)) {
+      return RULES_MAP_PETELKA.get(stateKey);
+    }
+
+//    n, nw, w, sw, s, se, e, ne
+    directions = new String[]{"N", "NW", "W", "SW", "S", "SE", "E", "NE"};
+    stateKey = getStateKey(cell, directions);
+    if (RULES_MAP_PETELKA.containsKey(stateKey)) {
+      return RULES_MAP_PETELKA.get(stateKey);
+    }
+
+//    (s, sw, w, nw, n, ne, e, se)
+    directions = new String[]{"S", "SW", "W", "NW", "N", "NE", "E", "SE"};
+    stateKey = getStateKey(cell, directions);
+    if (RULES_MAP_PETELKA.containsKey(stateKey)) {
+      return RULES_MAP_PETELKA.get(stateKey);
+    }
+
+//    w, nw, n, ne, e, se, s, sw
+    directions = new String[]{"W", "NW", "N", "NE", "E", "SE", "S", "SW"};
+    stateKey = getStateKey(cell, directions);
+    if (RULES_MAP_PETELKA.containsKey(stateKey)) {
+      return RULES_MAP_PETELKA.get(stateKey);
+    }
+
 
     // it is a valid length, then it returns 0 with Petelka's
     return 0;
   }
-
-  private String[] rotateClockwise(String[] directions) {
-    return new String[]{directions[7], directions[0], directions[1], directions[2],
-        directions[3], directions[4], directions[5], directions[6]};
-  }
-
-  private String[] reflectHorizontally(String[] directions) {
-    return new String[]{directions[0], directions[7], directions[6], directions[5],
-        directions[4], directions[3], directions[2], directions[1]};
-  }
-
 
 }
